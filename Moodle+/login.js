d3f904ac206fe3a0cdb74510a0bcf2dad796795b
Ayushman kumar singh <97663551+me1211004@users.noreply.github.com>
2022-03-24 18:33:43 +0530

    Add files via upload

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,33 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.getElementById("login"); 
 let login_text = login_element.innerText;
-
-let question = ""; // Use split and array operations on the login_text string to extract the question
-
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
-
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
-captcha_input_element.value = answer;
+let arr = login_text.split("\n");
+let question = arr[3]; 
+let s=question.split(" ");
+let answer = 0; 
+let b=0;
+let a=0;
+if (s[1] === "add")
+{
+    a=parseInt(s[2]);
+    b=parseInt(s[4]);
+    answer=a+b;
+}
+else if (s[1] === "subtract")
+{
+    a=parseInt(s[2]);
+    b=parseInt(s[4]);
+    answer=a-b;
+}
+else if (s[2] === "second")
+{
+    a=parseInt(s[6]);
+    answer=a;
+}
+else
+{
+    a=parseInt(s[4]);
+    answer = a;
+}
+const captcha_input_element = document.getElementById("valuepkg3"); 
+captcha_input_element.value = answer||0;
